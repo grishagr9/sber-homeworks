3c29f0bc439e7baa7071f9166c4c66788ba9711c
grishagr9 <grisha-rnd10@yandex.ru>
2024-01-04 19:09:41 +0300

    task 2 is done

--- src/main/java/org/example/hw4/Main.java
+++ src/main/java/org/example/hw4/Main.java
@@ -1,18 +1,44 @@
 package org.example.hw4;
 
-import javax.security.auth.login.AccountLockedException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void readContent(String url){
-
+    public static boolean readContent(String url) throws IOException, MalformedURLException {
+        URL website = new URL(url);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(website.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+        return true;
     }
     static void task1(){
         TerminalImpl terminal = new TerminalImpl();
         terminal.StartWork();
     }
-    public static void main(String[] args) {
-        task1();
+    static void task2(){
+        System.out.println("Введите url адрес страницы ");
+        Scanner scanner = new Scanner(System.in);
+        boolean result = false;
+        while (!result){
+            try {
+                String url = scanner.nextLine();
+                result = readContent(url);
+            }catch (IOException ioException){
+                System.err.println(ioException.getMessage());
+                System.out.print("Введите url адрес страницы еще раз");
+            }
+        }
+    }
+    public static void main(String[] args)  {
+        //task1();
+        //task2();
     }
 }
