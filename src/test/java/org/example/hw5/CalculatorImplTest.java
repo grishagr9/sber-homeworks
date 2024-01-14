f57dfffb9067ce3d3c513497553454cc4b268a9c
grishagr9 <grisha-rnd10@yandex.ru>
2024-01-14 11:18:21 +0300

    add test on calculator

--- src/test/java/org/example/hw5/CalculatorImplTest.java
+++ src/test/java/org/example/hw5/CalculatorImplTest.java
@@ -1,7 +1,17 @@
 package org.example.hw5;
 
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorImplTest {
 
+    @SneakyThrows
+    @Test
+    void calc() {
+        Calculator calculator = new CalculatorImpl();
+        Assertions.assertEquals(120, calculator.calc(5));
+    }
 }
