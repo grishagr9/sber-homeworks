e808396d49df4cc2854db56064d8abb68d926d78
grishagr9 <grisha-rnd10@yandex.ru>
2024-01-25 00:03:39 +0300

    create tests for Streams

--- src/test/java/org/example/hw9/StreamsTest.java
+++ src/test/java/org/example/hw9/StreamsTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,7 +24,7 @@ class StreamsTest {
     }
 
     @Test
-    void of() {
+    void givenPersonCollection_whenAllConditional_thenReturnMap() {
         Map<?, Person> m = Streams.of(someCollection).
                 filter(p->p.getAge()>20).
                 transform( p -> new Person(p.getName(),p.getAge() + 30))
@@ -33,18 +34,67 @@ class StreamsTest {
     }
 
     @Test
-    void filter() {
+    void givenPersonCollection_whenAllConditional_thenReturnTwoMapElement() {
+        Map<?, Person> m = Streams.of(someCollection).
+                filter(p->p.getAge()>20).
+                transform( p -> new Person(p.getName(),p.getAge() + 30))
+                .toMap(Person::getName, p->p);
+
+        Assertions.assertEquals(2, m.size());
     }
 
     @Test
-    void transform() {
+    void givenEmptyPersonCollection_whenAllCondition_thenReturnEmptyMap() {
+        Map<?, Person> m = Streams.of(new ArrayList<Person>()).
+                filter(p->p.getAge()>20).
+                transform( p -> new Person(p.getName(),p.getAge() + 30))
+                .toMap(Person::getName, p->p);
+
+        Assertions.assertEquals(0, m.size());
     }
 
     @Test
-    void toMap() {
+    void givenCollection_whenCallOf_thenReturnStreamsClass() {
+        Streams<Person> m = Streams.of(someCollection);
+
+        Assertions.assertEquals("Streams",m.getClass().getSimpleName());
     }
 
     @Test
-    void toList() {
+    void givenIntegerCondition_whenFiltering_thenReturnNewCollection() {
+        //given
+        Predicate<Integer> condition = new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer > 0;
+            }
+        };
+        List<Integer> collection = List.of(1, -2, -3, -4, -5, 6);
+
+        //when
+        var m = Streams.of(collection).
+                filter(condition).toList().toArray(new Integer[0]);
+
+        //then
+        Assertions.assertArrayEquals(new Integer[]{1,6}, m);
+    }
+
+    @Test
+    void givenStringCondition_whenFiltering_thenReturnNewCollection() {
+        //given
+        Predicate<String> condition = new Predicate<String>() {
+            @Override
+            public boolean test(String str) {
+                return str.startsWith("ka");
+            }
+        };
+        List<String> collection = List.of("emma", "kate", "karine", "bob", "kostya", "kamen");
+
+        //when
+        var m = Streams.of(collection).
+                filter(condition).toList().toArray(new String[0]);
+
+        //then
+        Assertions.assertArrayEquals(new String[]{"kate", "karine", "kamen"}, m);
     }
 }
