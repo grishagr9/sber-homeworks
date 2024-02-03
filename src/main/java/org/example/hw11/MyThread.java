0798514e58d2d5f32b00715c288b4b4e31e82009
grishagr9 <grisha-rnd10@yandex.ru>
2024-02-03 13:04:35 +0300

    create ScalableThreadPool

--- src/main/java/org/example/hw11/MyThread.java
+++ src/main/java/org/example/hw11/MyThread.java
@@ -3,6 +3,6 @@ package org.example.hw11;
 public class MyThread extends Thread{
     @Override
     public void run() {
-
+        System.out.println("doWork");
     }
 }
--- src/main/java/org/example/hw11/ScalableThreadPool.java
+++ src/main/java/org/example/hw11/ScalableThreadPool.java
@@ -1,13 +1,46 @@
 package org.example.hw11;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ScalableThreadPool implements ThreadPool{
+
+    private final int minThreads;
+    private final int maxThreads;
+
+    private final Queue<Runnable> taskQueue;
+
+    private int numThreads;
+
+    public ScalableThreadPool(int minThreads, int maxThreads) {
+        this.minThreads = minThreads;
+        this.maxThreads = maxThreads;
+        this.taskQueue = new LinkedList<>();
+        this.numThreads = minThreads;
+
+        for (int i = 0; i < minThreads; i++) {
+            new MyThread().start();
+        }
+    }
+
     @Override
     public void start() {
-
+        synchronized (taskQueue){
+            taskQueue.notifyAll();
+        }
     }
 
     @Override
     public void execute(Runnable runnable) {
+        synchronized (taskQueue){
+            taskQueue.add(runnable);
 
+            if(numThreads == minThreads){
+                numThreads++;
+                new MyThread().start();
+            }
+
+            taskQueue.notify();
+        }
     }
 }
