f4eea352689135d715d7637db3040747ecae5a31
grishagr9 <grisha-rnd10@yandex.ru>
2024-02-20 23:31:19 +0300

    task 1 ready

--- src/main/java/org/example/hw14/Task.java
+++ src/main/java/org/example/hw14/Task.java
@@ -2,14 +2,39 @@ package org.example.hw14;
 
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public class Task<T> {
+
+    private final Callable<? extends T> callable;
+    private volatile FutureTask<T> futureTask;
+
     public Task(Callable<? extends T> callable) {
-        //todo
+        this.callable = callable;
     }
 
     public T get() {
-         // todo implement me
-        return null;
+        if (futureTask == null) {
+            FutureTask<T> newTask = new FutureTask<>(() -> {
+                try {
+                    return callable.call();
+                } catch (Exception e) {
+                    throw new TaskExecutionException("Error executing task", e);
+                }
+            });
+
+            synchronized (this) {
+                if (futureTask == null) {
+                    futureTask = newTask;
+                    newTask.run();
+                }
+            }
+        }
+        try {
+            return futureTask.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new TaskExecutionException("Error getting task result", e);
+        }
     }
 }
---
+++ src/main/java/org/example/hw14/TaskExecutionException.java
@@ -0,0 +1,7 @@
+package org.example.hw14;
+
+public class TaskExecutionException extends RuntimeException{
+    public TaskExecutionException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
