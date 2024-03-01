d30ed5cbb2dafadb8403661ec2a53a375c9fb7c3
grishagr9 <grisha-rnd10@yandex.ru>
2024-03-01 15:07:35 +0300

    add tests

---
+++ HW15/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+//        for (int i = 0; i < 100000; i++) {
+//            map.put(i, "value" + i);
+//        }
+        System.out.println(1);
+    }
+}
